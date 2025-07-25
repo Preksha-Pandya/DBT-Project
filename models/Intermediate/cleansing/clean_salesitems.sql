c3e051d3ae87116acd8979f2910275e61531ab80
prekshapandya001@gmail.com <prekshapandya001@gmail.com>
2025-07-25 12:19:09 +0000

    changed the intermediate files

--- models/Intermediate/cleansing/clean_salesitems.sql
+++ models/Intermediate/cleansing/clean_salesitems.sql
@@ -1,4 +1,4 @@
-select item_id,sale_id,product_id,quantity,original_price,unit_price,
+select item_id,sale_id,product_id,quantity,original_price,unit_price,discount_applied,
 replace(discount_percent,'%',' ')::number(5,2) as discount_percent,discounted,
 item_total,sale_date,
 channel,channel_campaigns
