c10fc65c07e7fabc33f0d6450b908fcb274e47b5
Preksha Pandya <prekshapandya001@gmail.com>
2025-07-29 06:05:49 +0000

    worked on intermediate models

--- models/Intermediate/int_customer_orders.sql
+++ models/Intermediate/int_customer_orders.sql
@@ -1,20 +1,3 @@
--- with customer_orders as (
---     select * from {{ ref('int_campaign_sales') }}
--- )
-
--- select 
---     customer_id,
---     customer_country,
---     count(distinct sale_id) as total_orders,
---     count(*) as total_items,
---     sum(item_total) as total_spent,
---     round(avg(item_total)) as avg_order_value,
---     max(sale_date) as last_purchase_date,
---     datediff('day', max(sale_date), current_date) as days_since_last_purchase
--- from customer_orders
--- group by customer_id, customer_country
--- order by customer_id
-
 {{ config(
     materialized='incremental',
     unique_key='customer_id',
@@ -41,3 +24,6 @@ select
 from customer_orders
 group by customer_id, customer_country
 order by customer_id
+
+
+
